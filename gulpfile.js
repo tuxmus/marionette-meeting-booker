df51315d0d18289300f7e3a2eb36c9daf8f6be72
Joanne Daudier <jdaudier@outlook.com>
2014-10-12 22:26:09 -0400

    chore: update gulp file to handle prod env

--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,26 @@ gulp.task('minifyCSS', function() {
     .pipe(gulp.dest('public/dist'));
 });
 
+gulp.task('build', function() {
+  return gulp.src([
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/pickadate/lib/picker.js',
+    'bower_components/pickadate/lib/picker.date.js',
+    'bower_components/pickadate/lib/picker.time.js',
+    'bower_components/pickadate/lib/legacy.js',
+    'bower_components/semantic-ui/build/packaged/javascript/semantic.min.js',
+    'bower_components/json2/json2.js,',
+    'bower_components/underscore/underscore.js',
+    'bower_components/backbone/backbone.js',
+    'bower_components/marionette/lib/backbone.marionette.js',
+    'bower_components/backbone.radio/build/backbone.radio.js',
+    'public/javascripts/**/*.js'
+  ])
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('public/stylesheets/**.styl', ['compileCSS', 'minifyCSS']);
   gulp.watch('public/javascripts/**/*.js', ['concatJS']);
